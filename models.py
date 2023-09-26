b958936d6ee3c88b50f7aab03b1fbc9e6314292a
Seth Emery <37260478+collaboratorsje@users.noreply.github.com>
2023-09-25 23:21:50 -0500

    nearly completed user auth

--- models.py
+++ models.py
@@ -1,34 +1,7 @@
 from app import db
 from flask_login import UserMixin
-
-### example model from tutorial
-
-class User(UserMixin, db.Model):
-    __tablename__ = "user"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    pwd = db.Column(db.String(300), nullable=False, unique=True)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
     
 ### models from app.py below
-
-class Users(UserMixin, db.Model):
-    __tablename__ = "user"
-    
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_fname = db.Column(db.String(25), nullable=False)
-    user_lname = db.Column(db.String(25), nullable=False)
-    user_email = db.Column(db.String(25), nullable=False)
-    role = db.Column(db.Integer, db.ForeignKey("Roles.role_id"), nullable=False)
-    major = db.Column(db.Integer, db.ForeignKey("Majors.major_id"), nullable=False)
-    degree = db.Column(db.Integer, db.ForeignKey("Degrees.degree_id"), nullable=False)
-    gpa = db.Column(db.Numeric(3,2))
-    hours = db.Column(db.Numeric(3,2))
-    graduating_semester = db.Column(db.Integer)
 
 class Roles(db.Model):
     role_id = db.Column(db.Integer, primary_key=True)
@@ -42,6 +15,25 @@ class Degrees(db.Model):
     degree_id = db.Column(db.Integer, primary_key=True)
     degree_name = db.Column(db.String(25), nullable=False)
 
+class Users(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_fname = db.Column(db.String(25), nullable=False)
+    user_lname = db.Column(db.String(25), nullable=False)
+    user_email = db.Column(db.String(25), unique=True, nullable=False)
+    # password is unique can change later
+    pwd = db.Column(db.String(300), nullable=False, unique=True)
+    role = db.Column(db.Integer, db.ForeignKey("roles.role_id"), nullable=False)
+    major = db.Column(db.Integer, db.ForeignKey("majors.major_id"), nullable=False)
+    degree = db.Column(db.Integer, db.ForeignKey("degrees.degree_id"), nullable=False)
+    gpa = db.Column(db.Numeric(3,2))
+    hours = db.Column(db.Numeric(3,2))
+    graduating_semester = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<User %r>' % self.user_email
+
 class Courses(db.Model):
     course_id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(25), nullable=False)
@@ -49,23 +41,23 @@ class Courses(db.Model):
 class Jobs(db.Model):
     job_id = db.Column(db.Integer, primary_key=True)
     job_name = db.Column(db.String(25), nullable=False)
-    course_required = db.Column(db.Integer, db.ForeignKey("Courses.course_id"))
+    course_required = db.Column(db.Integer, db.ForeignKey("courses.course_id"))
     certification_required = db.Column(db.Boolean, nullable=False)
     status = db.Column(db.Boolean, nullable=False)
 
 class Certifications(db.Model):
     cert_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.user_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
 class UserCourses(db.Model):
     uc_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.user_id"))
-    course_id = db.Column(db.Integer, db.ForeignKey("Courses.course_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.course_id"))
     grade = db.Column(db.Numeric(5,2), nullable=False)
 
 class Applications(db.Model):
     app_id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.user_id"))
-    course_id = db.Column(db.Integer, db.ForeignKey("Courses.course_id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    course_id = db.Column(db.Integer, db.ForeignKey("courses.course_id"))
     status = db.Column(db.Boolean, nullable=False)
     editable = db.Column(db.Boolean, nullable=False)
---
+++ routes.py
@@ -0,0 +1,58 @@
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    flash,
+    url_for,
+    session
+)
+
+from datetime import timedelta
+from sqlalchemy.exc import (
+    IntegrityError,
+    DataError,
+    DatabaseError,
+    InterfaceError,
+    InvalidRequestError,
+)
+from werkzeug.routing import BuildError
+
+
+from flask_bcrypt import Bcrypt,generate_password_hash, check_password_hash
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+from app import create_app,db,login_manager,bcrypt
+from models import Users
+from forms import login_form,register_form
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+app = create_app()
+
+@app.before_request
+def session_handler():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=1)
+
+@app.route('/')
+def Home():
+    return render_template("index.html")
+
+@app.route('/login')
+def Login():
+    return render_template("login.html")
+
+@app.route('/register')
+def Register():
+    return render_template("register.html")
