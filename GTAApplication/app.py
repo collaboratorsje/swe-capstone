1fe7044a5cf1c2e408e34fb29c8b8c94bdbe558a
Seth Emery <37260478+collaboratorsje@users.noreply.github.com>
2023-09-28 12:40:29 -0500

    Update app.py

--- GTAApplication/app.py
+++ GTAApplication/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from GTAApplication import gta, db, login_manager  #, models, forms
 from GTAApplication.forms import forms
