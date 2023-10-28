49d0630e1a042c15c13aeeb2e66880023ae97224
Seth Emery <37260478+collaboratorsje@users.noreply.github.com>
2023-10-28 01:13:40 -0500

    Additional style changes
    Made flash messages have the ability to be dismissed and made more style changes to the profile page.

--- gta/static/scripts.js
+++ gta/static/scripts.js
@@ -2,12 +2,13 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-        let alerts = document.querySelectorAll('.alert');
+        let alerts = document.querySelectorAll('.alert:not(.static-alert)');
         alerts.forEach(function(alert) {
             alert.style.opacity = '0';
             setTimeout(function() {
                 alert.style.display = 'none';
             }, 1000); // Matches the duration of the fade effect
         });
-    }, 7500); // Duration until the fade-out starts
+    }, 5000); // Duration until the fade-out starts
 });
+
