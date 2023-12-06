521ad4108d083252f951c63b4702d39a85142604
bclord115 <135069371+bclord115@users.noreply.github.com>
2023-12-06 16:20:45 -0600

    admin applications fix + test cases

--- tests/test_user_actions.py
+++ tests/test_user_actions.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page, expect
 import re
-from test_util import login, click_login, click_register, delete_test_user
+from test_util import login, click_login, click_register, delete_test_user, handle_dialog
 
 base = "http://localhost:5000"
 apps = base+'/applications'
@@ -13,11 +13,13 @@ prof = base+'/profile'
 def test_unlogged_click_login(page: Page):
     click_login(page)
     expect(page).to_have_url(base+'/')
+    # check for profile button
 
 # 2.1
 def test_unlogged_click_register(page: Page):
     click_register(page)
     expect(page).to_have_url(lin)
+    # check user in db
     delete_test_user()
 
 # 2.1
@@ -25,6 +27,8 @@ def test_unlogged_click_register_login(page: Page):
     click_register(page)
     click_login(page, user="testtest@test.com")
     expect(page).to_have_url(base+'/')
+    # check user in db
+    # check for profile button
     delete_test_user()
 
 # 2.3
@@ -32,9 +36,51 @@ def test_logged_click_logout(page:Page):
     login(page)
     page.locator('li[name="nav-logout"]').click()
     expect(page).to_have_url(base+'/')
+    # check for login button
 
 # 2.1
-#def test_logged_click_register_ucourses(page: Page):
+def test_logged_click_register_ucourses(page: Page):
+    click_register(page, courses=True)
+    click_login(page, user="testtest@test.com")
+    expect(page).to_have_url(base+'/')
+    # check user in db
+    # check for profile button
+    delete_test_user()
+
+# 5.1
+def test_filter_course(page: Page):
+    page.goto(base)
+    page.select_option('select#course-filter, 'CS 201R - Problem Solving and Programming II')
+    # check for courses not filtered
+
+# 5.2 
+def test_filter_position(page: Page):
+    page.goto(base)
+    page.select_option('select#role-filter', 'Grader')
+    # check for roles besides grader
+
+# 5.3 
+def test_filter_cert(page: Page):
+    page.goto(base)
+    page.selector(input[id="cert-filter"]').check()
+    # check for non-cert jobs listed
+
+# 5.4 
+def test_filter_search(page: Page):
+    page.goto(base)
+    page.selector(input[id="query"]').fill('CS 201R')
+    page.selector(button[id=""]').click()
+    # check for non CS 201R jobs
+
+# 5.5 
+def test_filter_combined(page: Page):
+    page.goto(base)
+    page.select_option('select#course-filter, 'CS 201R - Problem Solving and Programming II')
+    page.select_option('select#role-filter', 'Grader')
+    page.selector(input[id="query"]').fill('CS 201R')
+    page.selector(button[id=""]').click()
+    # check for non CS 201R jobs
+
 # 6.1
 def test_logged_click_apply_nocert(page: Page):
     login(page)
@@ -46,23 +92,7 @@ def test_logged_click_apply_nocert(page: Page):
     transcript_choice.set_files("testing-requirements.txt")
     page.get_by_role('button', name='submit').click()
     expect(page).to_have_url(base+'/')
-
-"""
-# 5.1
-def test_filter_course(page: Page):
-
-# 5.2 
-def test_filter_position(page: Page):
-
-# 5.3 
-def test_filter_cert(page: Page):
-
-# 5.4 
-def test_filter_search(page: Page):
-
-# 5.5 
-def test_filter_combined(page: Page)
-"""
+    # check for app in my applications
 
 # 6.2
 def test_logged_click_apply_cert(page: Page):
@@ -80,6 +110,7 @@ def test_logged_click_apply_cert(page: Page):
     gta_choice.set_files("testing-requirements.txt")
     page.get_by_role('button', name='submit').click()
     expect(page).to_have_url(base+'/')
+    # check for app in my applications
 
 # 6.3
 #def test_logged_click_edit_app(page: Page):
@@ -87,22 +118,51 @@ def test_logged_click_apply_cert(page: Page):
 # 6.4
 # def test_logged_click_edit_profile(page: Page):
 
-"""
 # 7.1
-def test_logged_click_edit_app(page: Page):
+def test_admin_click_edit_app(page: Page):
+    login(page, user="admin@admin.com", pa="admin")
+    page.goto(admin)
+    page.locator(button[id="job-oc"]').click()
+    # check if job is open
 
 # 7.2
 def test_admin_click_edit_job(page: Page):
+    login(page, user="admin@admin.com", pa="admin")
+    page.goto(admin)
+    page.locator(button[id="job-edit"]').click()
+    # Make some edit
+    # Save edit
+    # check job edited
 
 # 7.3
 def test_admin_click_remove_job(page: Page):
+    login(page, user="admin@admin.com", pa="admin")
+    page.goto(admin)
+    page.locator(button[id="job-remove"]').click()
+    page.on("dialog", handle_dialog)
+    # check job is removed
 
 # 7.4
 def test_admin_click_accept_app(page: Page):
+    login(page, user="admin@admin.com", pa="admin")
+    page.goto(admin)
+    page.locator('a[id="applicationsButton"]').click()
+    page.locator('button[id="app-acc"]').click()
+    # check if app is accepted
 
 # 7.5
 def test_admin_click_reject_app(page: Page):
-
+    login(page, user="admin@admin.com", pa="admin")
+    page.goto(admin)
+    page.locator('a[id="applicationsButton"]').click()
+    page.locator('button[id="app-rej"]').click()
+    # check if app is rejected
 # 7.6
 def test_admin_click_create_job(page: Page):
-"""
+    login(page, user="admin@admin.com", pa="admin")
+    page.goto(admin)
+    page.locator('a[id="create-job"]').click()
+    page.select_option('select#role', 'Grader')
+    page.select_option('select#course_required', 'CS 101 - Problem Solving and Programming I')
+    page.get_by_role('button', 'submit').click()
+    # check if job present
--- tests/test_util.py
+++ tests/test_util.py
@@ -11,7 +11,7 @@ def login(page: Page, user="testuc@test.com", pa="test"):
     page.locator('input[name="user_pass"]').fill(pa)
     page.get_by_role('button', name='Submit').click()
 
-def click_register(page: Page, user="testtest@test.com", pa="test", id="000000001", fname="test", lname="test", gpa="4", hours="40"):
+def click_register(page: Page, user="testtest@test.com", pa="test", id="000000001", fname="test", lname="test", gpa="4", hours="40", courses=None):
     page.goto(base)
     page.locator('li[name="nav-register"]').click()
     page.locator('input[name="user_id"]').fill(id)
@@ -24,6 +24,12 @@ def click_register(page: Page, user="testtest@test.com", pa="test", id="00000000
     page.locator('input[name="user_hours"]').fill(hours)
     page.locator('input[name="user_pass"]').fill(pa)
     page.locator('input[name="user_confirm_pass"]').fill(pa)
+    if courses:
+        page.get_by_role('button', name='adducourse').click()
+        page.select_option('select#course_id', "CS 101 - Problem Solving and Programming I")
+        page.select_option('select#grade', "A")
+        page.locator('input[id="courseSubmit"]').click()
+        page.locator('button[class="btn-close"]').click()
     page.get_by_role('button', name="submit").click()
 
 def click_login(page: Page, user="testuc@test.com", pa="test"):
@@ -38,4 +44,6 @@ def delete_test_user():
     dbfile = os.path.join(cw, "database.db")
     with sqlite3.connect(dbfile) as db:
         db.cursor().execute("""DELETE FROM Users WHERE user_email='testtest@test.com'""")
-    
+
+def handle_dialog(d):
+    d.accept()
